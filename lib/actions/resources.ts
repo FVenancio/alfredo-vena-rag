6c5770fa3be9e4edb3892526c5ced8ab48d710dc
Francisco Venâncio <franciscovenancio1997@gmail.com>
2024-08-19 16:56:40 +0100

    Basic RAG demo with tools to insert and query

--- lib/actions/resources.ts
+++ lib/actions/resources.ts
@@ -1,25 +1,25 @@
 "use server";
 
-import {
-  NewResourceParams,
-  insertResourceSchema,
-  resources,
-} from "@/lib/db/schema/resources";
+import { NewResourceParams, insertResourceSchema, resources } from "@/lib/db/schema/resources";
 import { db } from "../db";
+import { generateEmbeddings } from "../ai/embedding";
+import { embeddings as embeddingsTable } from "../db/schema/embeddings";
 
 export const createResource = async (input: NewResourceParams) => {
   try {
-    const payload = insertResourceSchema.parse(input);
+    const { content } = insertResourceSchema.parse(input);
 
-    const contentWithoutLineBreaks = payload.content.replace("\n", " ");
-    const [resource] = await db
-      .insert(resources)
-      .values({ content: contentWithoutLineBreaks })
-      .returning();
+    const [resource] = await db.insert(resources).values({ content }).returning();
+    const embeddings = await generateEmbeddings(content);
+    await db.insert(embeddingsTable).values(
+      embeddings.map((embedding) => ({
+        resourceId: resource.id,
+        ...embedding,
+      }))
+    );
 
-    return "Resource successfully created.";
+    return "Resource successfully created and embedded.";
   } catch (e) {
-    if (e instanceof Error)
-      return e.message.length > 0 ? e.message : "Error, please try again.";
+    if (e instanceof Error) return e.message.length > 0 ? e.message : "Error, please try again.";
   }
 };
