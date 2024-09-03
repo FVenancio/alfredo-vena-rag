9a592f08603221791595c94b4530a5e328b98434
Francisco Venâncio <franciscovenancio1997@gmail.com>
2024-09-03 13:07:35 +0100

    Updated for readability

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,9 +9,17 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  const initial_prompt = `
+You are an AI assistant tasked with reformulating user queries to improve retrieval in a RAG system. 
+Given the original query, rewrite it to be more specific, detailed, and likely to retrieve relevant information. 
+After you have rewritten the query, check your knowledge base before answering any questions. 
+Only respond to questions using information from tool calls. 
+If no relevant information is found in the tool calls, apology and answer that you don't have that information available. 
+Always answer back using the same language the user spoke in. If the user used Portuguese, answer back using Portuguese from Portugal.`;
+
   const result = await streamText({
     model: openai("gpt-4o-mini"),
-    system: `You are an AI assistant tasked with reformulating user queries to improve retrieval in a RAG system. Given the original query, rewrite it to be more specific, detailed, and likely to retrieve relevant information. After you have rewritten the query, check your knowledge base before answering any questions. Only respond to questions using information from tool calls. If no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
+    system: initial_prompt,
     messages: convertToCoreMessages(messages),
     tools: {
       getInformation: tool({
